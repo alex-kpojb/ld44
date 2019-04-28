90d5d513ec300992786e6f16b4390a46fec55cd9
tulipekoni <34221969+tulipekoni@users.noreply.github.com>
2019-04-28 18:23:42 +0300

    bullet added

---
+++ ld44/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject pausePanel;
+    void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (!pausePanel.activeInHierarchy)
+            {
+                PauseGame();
+            }
+            if (pausePanel.activeInHierarchy)
+            {
+                ContinueGame();
+            }
+        }
+    }
+    private void PauseGame()
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+    }
+    public void ContinueGame()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+    }
+}
