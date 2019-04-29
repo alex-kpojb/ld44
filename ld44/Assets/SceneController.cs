fa0f18fddaa3a4f3fe45ac9ab23e41c61ac175da
novehube <novehube@binnary.com>
2019-04-29 17:53:44 +0300

    working FadeOut()

--- ld44/Assets/SceneController.cs
+++ ld44/Assets/SceneController.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class SceneController : MonoBehaviour
 {
     public static SceneController instance = null;
 
+    public TMP_Text text1;
+    public TMP_Text text2;
+    public TMP_Text text3;
+    public TMP_Text text4;
+    public TMP_Text text5;
+    public TMP_Text text6;
+    public TMP_Text text7;
+    public SpriteRenderer fadeImage;
+
+    bool isGameOver = false;
 
     private void Awake() {
         if (instance == null)
@@ -34,11 +45,38 @@ public class SceneController : MonoBehaviour
 
 
     public void GameOver() {
+        if (isGameOver)
+            return;
+
+        isGameOver = true;
         StartCoroutine(GameOverCoroutine());     
     }
 
-    IEnumerator Fade(Image componentToFade, float from, float to, float lerpTime) {
-        //using basic Lerp logic here
+
+    IEnumerator FadeIn() {
+        StartCoroutine(Fade(text1, 0, 255, 1f));
+        StartCoroutine(Fade(text2, 0, 255, 1f));
+        StartCoroutine(Fade(text3, 0, 255, 1f));
+        StartCoroutine(Fade(text4, 0, 255, 1f));
+        StartCoroutine(Fade(text5, 0, 255, 1f));
+        StartCoroutine(Fade(text6, 0, 255, 1f));
+        StartCoroutine(Fade(text7, 0, 255, 1f));
+        yield return StartCoroutine(Fade(fadeImage, 255, 0, 1f));
+    }
+    IEnumerator FadeOut() {
+        StartCoroutine(Fade(text1, 255, 0, 1f));
+        StartCoroutine(Fade(text2, 255, 0, 1f));
+        StartCoroutine(Fade(text3, 255, 0, 1f));
+        StartCoroutine(Fade(text4, 255, 0, 1f));
+        StartCoroutine(Fade(text5, 255, 0, 1f));
+        StartCoroutine(Fade(text6, 255, 0, 1f));
+        StartCoroutine(Fade(text7, 255, 0, 1f));
+        yield return StartCoroutine(Fade(fadeImage, 0, 255, 1f));
+        //yield return new WaitForSeconds(2f);
+    }
+
+    IEnumerator Fade(TMP_Text componentToFade, float from, float to, float lerpTime) {
+
         float t = 0f;
         Color32 cachedColor = componentToFade.color;
 
@@ -46,8 +84,6 @@ public class SceneController : MonoBehaviour
             if (t >= lerpTime)
                 t = lerpTime;
 
-            //change alpha value and assign the value
-            //the core of the function
             cachedColor.a = (byte)Mathf.Lerp(from, to, t / lerpTime);
             componentToFade.color = cachedColor;
 
@@ -56,11 +92,32 @@ public class SceneController : MonoBehaviour
             t += Time.deltaTime;
             yield return null;
         }
+        yield return null;
     }
+    IEnumerator Fade(SpriteRenderer componentToFade, float from, float to, float lerpTime) {
 
+        float t = 0f;
+        Color32 cachedColor = componentToFade.color;
+
+        while (true) {
+            if (t >= lerpTime)
+                t = lerpTime;
+
+            var a = Mathf.Lerp(from, to, t / lerpTime);
+            cachedColor.a = (byte)Mathf.Lerp(from, to, t / lerpTime);
+            componentToFade.color = cachedColor;
+
+            if (t >= lerpTime)
+                break;
+            t += Time.deltaTime;
+            yield return null;
+        }
+        yield return null;
+    }
     IEnumerator GameOverCoroutine() {
         Time.timeScale = 0.5f;
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(0.2f);
+        yield return StartCoroutine(FadeOut());
         toScene(0);
         yield return null;
     }
---
+++ ld44/Assets/Scripts/TextWobbling.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextWobbling : MonoBehaviour
+{
+    public float AngleMultiplier = 1.0f;
+    public float SpeedMultiplier = 0.2f;
+    public float CurveScale = 1.0f;
+
+    TMP_Text textComponent;
+    bool hasTextChanged;
+
+    struct VertexAnim
+    {
+        public float angleRange;
+        public float angle;
+        public float speed;
+    }
+
+    void Awake()
+    {
+        textComponent = GetComponent<TMP_Text>();
+        TMPro_EventManager.TEXT_CHANGED_EVENT.Add(ON_TEXT_CHANGED);
+    }
+
+    private void OnEnable() {
+        StartCoroutine(AnimateVertex());
+    }
+
+    private void OnDestroy() {
+        TMPro_EventManager.TEXT_CHANGED_EVENT.Remove(ON_TEXT_CHANGED);
+    }
+
+    void ON_TEXT_CHANGED(Object obj) {
+        if (obj == textComponent) 
+            hasTextChanged = true;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    IEnumerator AnimateVertex() {
+        textComponent.ForceMeshUpdate();
+
+        var textInfo = textComponent.textInfo;
+
+        Matrix4x4 matrix;
+
+        int loopCount = 0;
+        hasTextChanged = true;
+
+        VertexAnim[] vertexAnim = new VertexAnim[1024];
+        for (int i =0; i<1024; i++) {
+            vertexAnim[i].angleRange = Random.Range(10f, 25f);
+            vertexAnim[i].speed = Random.Range(1f, 3f);
+        }
+
+        TMP_MeshInfo[] cachedMeshInfo = textInfo.CopyMeshInfoVertexData();
+
+        while (true) {
+            if (hasTextChanged) {
+                cachedMeshInfo = textInfo.CopyMeshInfoVertexData();
+                hasTextChanged = false;
+            }
+
+            int characterCount = textInfo.characterCount;
+
+            for (int i = 0; i < characterCount; i++) {
+                var charInfo = textInfo.characterInfo[i];
+
+                if (!charInfo.isVisible)
+                    continue;
+
+                VertexAnim vertAnim = vertexAnim[i];
+                int materialIndex = textInfo.characterInfo[i].materialReferenceIndex;
+                int vertexIndex = textInfo.characterInfo[i].vertexIndex;
+
+                Vector3[] sourceVertices = cachedMeshInfo[materialIndex].vertices;
+
+                Vector2 charMidBaseLine = (sourceVertices[vertexIndex + 0] + sourceVertices[vertexIndex + 2]) / 2;
+
+                Vector3 offset = charMidBaseLine;
+
+                Vector3[] destinationVertices = textInfo.meshInfo[materialIndex].vertices;
+
+                destinationVertices[vertexIndex + 0] = sourceVertices[vertexIndex + 0] - offset;
+                destinationVertices[vertexIndex + 1] = sourceVertices[vertexIndex + 1] - offset;
+                destinationVertices[vertexIndex + 2] = sourceVertices[vertexIndex + 2] - offset;
+                destinationVertices[vertexIndex + 3] = sourceVertices[vertexIndex + 3] - offset;
+
+                vertAnim.angle = Mathf.SmoothStep(
+                    -vertAnim.angleRange,
+                    vertAnim.angleRange,
+                    Mathf.PingPong(loopCount / 25f * vertAnim.speed, 1f));
+
+                Vector3 wobblingOffset = new Vector3(
+                    Random.Range(-0.25f, 0.25f),
+                    Random.Range(-0.25f, 0.25f), 0);
+
+                matrix = Matrix4x4.TRS(
+                    wobblingOffset * CurveScale,
+                    Quaternion.Euler(0, 0, Random.Range(-5f, 5f) * AngleMultiplier),
+                    Vector3.one);
+
+                destinationVertices[vertexIndex + 0] = matrix.MultiplyPoint3x4(destinationVertices[vertexIndex + 0]);
+                destinationVertices[vertexIndex + 1] = matrix.MultiplyPoint3x4(destinationVertices[vertexIndex + 1]);
+                destinationVertices[vertexIndex + 2] = matrix.MultiplyPoint3x4(destinationVertices[vertexIndex + 2]);
+                destinationVertices[vertexIndex + 3] = matrix.MultiplyPoint3x4(destinationVertices[vertexIndex + 3]);
+
+                destinationVertices[vertexIndex + 0] += offset;
+                destinationVertices[vertexIndex + 1] += offset;
+                destinationVertices[vertexIndex + 2] += offset;
+                destinationVertices[vertexIndex + 3] += offset;
+
+                vertexAnim[i] = vertAnim;
+            }
+
+            for (int i = 0; i < textInfo.meshInfo.Length; i++) {
+                textInfo.meshInfo[i].mesh.vertices = textInfo.meshInfo[i].vertices;
+                textComponent.UpdateGeometry(textInfo.meshInfo[i].mesh, i);
+            }
+
+            loopCount += 1;
+
+            yield return new WaitForSeconds(1f * SpeedMultiplier);
+        }
+    }
+}
